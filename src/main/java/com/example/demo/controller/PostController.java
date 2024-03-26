13a46389b7ba081c4ea788989c8fbf3e720363ce
p_lgcns_00390 <p.choifree93@kalmate.net>
2024-03-26 15:24:36 +0900

    first commit

---
+++ src/main/java/com/example/demo/controller/PostController.java
@@ -0,0 +1,22 @@
+package com.example.demo.controller;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/v1/post")
+public class PostController {
+
+    @GetMapping("/getPosts")
+    public ResponseEntity<?> getPosts() {
+        return new ResponseEntity<>(HttpStatus.OK);
+    }
+
+    @GetMapping("/health")
+    public String health() {
+        return "health check post controller";
+    }
+}
