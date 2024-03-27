d7b23bc3b0e588f2285296f05b9bc46257e33a59
choifree <p.choifree93@kalmate.net>
2024-03-27 16:44:21 +0900

    jpa 연결 및 select, insert api 작업

--- src/main/java/com/example/demo/controller/PostController.java
+++ src/main/java/com/example/demo/controller/PostController.java
@@ -1,18 +1,36 @@
 package com.example.demo.controller;
 
+import com.example.demo.dto.RequestPostDto;
+import com.example.demo.dto.ResponseDto;
+import com.example.demo.entity.PostEntity;
+import com.example.demo.service.PostService;
+import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
+
+@RequiredArgsConstructor
 @RestController
 @RequestMapping("/api/v1/post")
-public class PostController {
+public class PostController extends BaseController {
+
+    private final PostService postService;
 
     @GetMapping("/getPosts")
     public ResponseEntity<?> getPosts() {
-        return new ResponseEntity<>(HttpStatus.OK);
+        return new ResponseEntity<>(postService.getPosts(), null, HttpStatus.OK);
+    }
+
+    @GetMapping("/getPostById/{id}")
+    public ResponseEntity<?> getPostById(@PathVariable("id") Long id) {
+        return new ResponseEntity<>(postService.getPostById(id), null, HttpStatus.OK);
+    }
+
+    @PostMapping("/writePost")
+    public ResponseEntity<?> writePost(@RequestBody RequestPostDto requestPostDto) {
+        return new ResponseEntity<>(postService.writePost(requestPostDto), null, HttpStatus.OK);
     }
 
     @GetMapping("/health")
